220013193051eaa0aada32685cdbc80b2b98933e
Vamshi Krishna <curiuskrish@.com>
2025-09-05 19:27:21 +0530

    Fixed bugs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "dist"
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
